02efdfc56f7180f191fc8a8fb127f989d246101c
wudi <532787678@qq.com>
2025-05-27 16:13:24 +0800

    feat: add oracle and test

--- src/Interfaces.sol
+++ src/Interfaces.sol
@@ -73,4 +73,8 @@ interface IUniswapV2Pair {
     function totalSupply() external view returns (uint);
     function balanceOf(address owner) external view returns (uint);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
 } 
